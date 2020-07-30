7763450ced4fa42d9b3f85959851217b1fe62b5c
Keriel <keriel@jupiterflow.com>
2020-07-30 13:39:22 +0900

    :tada: Begin a project

---
+++ src/main/java/kr/dataportal/cms/controller/IndexController.java
@@ -0,0 +1,11 @@
+package kr.dataportal.cms.controller;
+
+import org.springframework.stereotype.Controller;
+
+// TODO: 2020-07-30 오후 1:39 IndexContoller 생성 -Jinssssun
+@Controller
+public class IndexController {
+    public String Index() {
+        return "index";
+    }
+}
